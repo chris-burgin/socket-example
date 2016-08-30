425396518509520c22f88bdde959c136b6fbe651
ChrisBurgin <chrisburgin95@gmail.com>
2016-08-30 17:11:34 -0400

    Added Icon

--- src/js/chat/modules/notifications.js
+++ src/js/chat/modules/notifications.js
@@ -7,7 +7,12 @@
 
     const send = (user, message) => {
       if (document.hasFocus() === false) {
-        let notification = new Notification(`${user}: ${message}`);
+        var options = {
+          body: `${user}: ${message}`,
+          icon: "https://secure.gravatar.com/avatar/b623fa7b975d0bf0832cc2b727bc3297.jpg"
+        }
+
+        let notification = new Notification('New Message!', options);
       }
     }
 
